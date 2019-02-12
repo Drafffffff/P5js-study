8f8bea0d0df99febced8bb6fd29ef7282fc4be44
longfei Zhou <i0.0ia@qq.com>
2019-02-13 01:43:39 +0800

    Phyllotaxis

--- Phyllotaxis/Phyllotaxis.js
+++ Phyllotaxis/Phyllotaxis.js
@@ -1,20 +1,42 @@
 var n = 0;
-var c  = 2;
+var c = 3.2;
+var Angle = 137.5;
+
 function setup() {
-    createCanvas(300, 300)
-    background(0);
+  createCanvas(300, 300);
+  angleMode(DEGREES);
+  background(0);
 
 }
 
 function draw() {
 
-  var a = n * 137.5;
-    var r =  c* sqrt(n);
-    var x = r * cos(a) + width/2;
-    var y  =r * sin (a)+height/2;
-fill(255);
-ellipse(x,  y, 4);
-n++;
+  var a = n * Angle;
+  var r = c * sqrt(n);
+  var x = r * cos(a) + width / 2;
+  var y = r * sin(a) + height / 2;
+  fill(255);
+  noStroke();
+  ellipse(x, y, 4);
+  n++;
 
-  
+}
+
+function getInputValue() {
+  var inputValue = document.getElementById("degree").value;
+  if (inputValue) {
+    return inputValue;
+  }
+}
+
+
+
+function restart() {
+
+  background(0);
+  n = 0;
+  if( getInputValue()){
+    Angle = getInputValue()
+  }
+
 }
